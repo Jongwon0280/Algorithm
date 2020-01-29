28b6e6cd063459a976b0f1ae5b92b05c7f7c1f89
Jongwon0280 <56438131+Jongwon0280@users.noreply.github.com>
2020-01-30 00:47:09 +0900

    Update nQueen.cpp

--- nQueen.cpp
+++ nQueen.cpp
@@ -1,30 +1,32 @@
 #include<stdio.h>
-int R[15],  L[15], U[15],  count, n;
+#include<Windows.h>
+int R[15], L[15], U[15], count, n;
 int map[100][100];
-void queen(int y, int x,int n,int R[],int L[],int U[]) { 
+void queen(int y, int x, int n, int R[], int L[], int U[]) {
 	if (R[y + x] || L[y - x + n] || U[x]) {
 		return;
 	}
 	else {
-		printf("(%d %d) ", y, x);
 		R[y + x] = L[y - x + n] = U[x] = 1;
 	}
 	if (y == n - 1) {
-		printf("\n\n\n");
 		count++;
 		R[y + x] = L[y - x + n] = U[x] = 0;
 		return;
 	}
 	for (int i = 0; i < n; i++) {
-		queen(y + 1, i,n,R,L,U);
+		queen(y + 1, i, n, R, L, U);
 	}
 	R[y + x] = L[y - x + n] = U[x] = 0;
 
 }
 int main()
 {
-	n = 10;
-	queen(0, 0,n,R,L,U);
-	printf("%d\n", count);
+	n = 4; //n by n
+	for (int i = 0; i < n; i++) { // column
+		queen(0, i, n, R, L, U);
+	}
+	printf("%d\n", count); // 경우의 수 
+	system("pause");
 	return 0;
 }
