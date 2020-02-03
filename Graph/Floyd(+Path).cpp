50b11a352717a88c011e60206150f5dca29b149e
Jongwon0280 <56438131+Jongwon0280@users.noreply.github.com>
2020-02-03 18:00:55 +0900

    Floyd(+Path)

--- Graph/Floyd(+Path).cpp
+++ Graph/Floyd(+Path).cpp
@@ -1,4 +1,4 @@
-/*#include<stdio.h>
+#include<stdio.h>
 int map[10][10];
 int F[10][10];
 void path(int q, int r) {
@@ -54,34 +54,5 @@ int main()
 	}
 	path(4,2);
 	printf("\n");
-	return 0;
-}*/
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-int things[4][2] = { { 0,0 },{ 5,50 },{ 10,60 },{ 20,140 } };
-int P[10][10];
-int Max(int a, int b) {
-	if (a > b)
-		return a;
-	else
-		return b;
-}
-int main()
-{
-	int n = 3;
-	int max = 30;
-	for (int i = 1; i <= 3; i++) { // ¹°°Ç
-		for (int j = 0; j <= max; j += 10) {
-			if (things[i][0] > j) P[i][j/10] = P[i - 1][j/10];
-			else P[i][j / 10] = Max(P[i - 1][j / 10], things[i][1] + P[i - 1][(j - things[i][0]) / 10]);
-		}
-	}
-	for (int i = 1; i <= 3; i++) {
-		for (int j = 1; j <= max / 10; j++) {
-			printf("%d ", P[i][j]);
-		}
-		printf("\n");
-	}
 	return 0;
 }
